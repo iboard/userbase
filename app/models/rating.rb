dddd55b3d8ccba4aa5e3f8ed99e04980a0582974
Andi Altendorfer <andi@iboard.cc>
2010-12-22 08:51:41 +0100

    No timestamp-update on rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -7,6 +7,8 @@ class Rating < ActiveRecord::Base
   private
   def update_average
     self.rateable.ratings_average = self.rateable.ratings.average(:rating) || 0
+    self.rateable.class.record_timestamps = false
     self.rateable.save!
+    self.rateable.class.record_timestamps = true
   end
 end
