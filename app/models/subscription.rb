d27c44f4845211c6a38270c0a26489bbc5defb8f
Andi Altendorfer <andi@iboard.cc>
2011-01-04 17:00:28 +0100

    Minor fixes with subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,19 +5,31 @@ class Subscription < ActiveRecord::Base
   
   
   def matching_blog_entries(user=nil,since=nil)
-    since ||= Time.new(1970,1,1)
-    unless self.blogable_type.blank? 
-      eval("#{self.blogable_type}.readable(#{user ? user.roles_mask : 1}).where('updated_at > ?',since)")
+    since ||= Time.new(1970)
+    tags = self.tag_list.split(/,/).map{|t| t.strip}.reject{|r| r.blank?}
+    
+    unless self.blogable_type.blank?
+      # Search by blogable_type (one resource)
+      blogables = eval("#{self.blogable_type}.readable(#{user ? user.roles_mask : 1})."+
+                       "where('updated_at > ?',since)").\
+                      order("updated_at desc")
+      if tags.any?
+        blogables = blogables.tagged_with(tags,:any => true).order("updated_at desc")
+      end
+      blogables.all
     else
-      Blogable::collect { |resource|
-          unless self.tag_list.blank?
-            resource.tagged_with(self.tag_list.split(/[,\s+]/).reject(&:blank?), :any => true).\
-              readable(user ? user.roles_mask : 1).where('updated_at > ?',since).all
-          else
-            resource.readable(user ? user.roles_mask : 1).where('updated_at > ?',since).all
-          end
+      # Collect from all resources
+      blogables = Blogable::collect { |resource|
+        if tags.any?
+          resource.tagged_with(tags, :any => true).\
+            readable(user ? user.roles_mask : 1).where('updated_at > ?',since).all
+        else
+          resource.readable(user ? user.roles_mask : 1).where('updated_at > ?',since).all
+        end
       }.flatten.sort {|b,a| a.updated_at <=> b.updated_at }
     end
+    
+    
   end
   
   def deliver_notifications(user=nil)
